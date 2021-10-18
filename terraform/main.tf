a226ffa53e9749d417ba17e0077deecf6ba41c64
gabriel <gabriel@example.com>
2021-10-18 18:28:22 -0300

    alteracao

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,66 +22,6 @@ resource "aws_instance" "maquina_nodejs" {
     vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name = "ssh_nodejs"
-  description = "Permitindo SSH para as maquina wordpress"
-  vpc_id      = "vpc-0a62871521ff123ee"
-
-  ingress = [
-    {
-      description      = "SSH from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
-    },
-    {
-      description      = "Acesso HTTPS"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
-    },
-    {
-      description      = "Acesso HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null
-    }
-  ]
-
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"],
-      prefix_list_ids = null,
-      security_groups: null,
-      self: null,
-      description: "Libera dados da rede interna"
-    }
-  ]
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
 output "aws_instance_e_ssh" {
   value = aws_instance.maquina_nodejs.public_dns
 }
---
+++ terraform/security_group_ssh.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "allow_ssh" {
+  name = "ssh_maquina_nodejs"
+  description = "Permitindo SSH para as maquinas criadas pelo terraform"
+  vpc_id      = "vpc-0a62871521ff123ee"
+
+ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    },
+    {
+      description      = "SSH from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    },
+    {
+      description      = "Acesso HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups = null,
+      self            = null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups  = null,
+      self             = null,
+      description      = "Libera dados da rede interna"
+    }
+  ]
+  
+  tags = {
+  Name = "allow_ssh"
+    }
+}
